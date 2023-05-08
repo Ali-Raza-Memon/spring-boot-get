f492d13e646958b22e5b7a2efb9d2b0bf1e4af7a
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-05-08 19:32:22 +0500

    New test Get method implemnented

--- src/main/java/com/springboot/demo/Application.java
+++ src/main/java/com/springboot/demo/Application.java
@@ -3,6 +3,7 @@ package com.springboot.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,16 @@ public class Application {
 	public Home getDetailById(@PathVariable String id){
 		return this.homeServices.getDataById(id);
 	}
+
+
+	@GetMapping("/test")
+	public Home showMsg(){
+		Home hm = new Home();
+		hm.setName("Next.Js");
+		hm.setDetail("For front End Web Developer can learn this.");
+		return hm;
+	}
+
 
 
 
