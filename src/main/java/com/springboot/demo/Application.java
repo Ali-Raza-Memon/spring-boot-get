0a5132a1623061a1723e3a3c493636a7ba94fcd1
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-05-08 19:55:26 +0500

    Added very basic code of POST method that returns the same text

--- src/main/java/com/springboot/demo/Application.java
+++ src/main/java/com/springboot/demo/Application.java
@@ -3,10 +3,7 @@ package com.springboot.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,12 @@ public class Application {
 		hm.setName("Next.Js");
 		hm.setDetail("For front End Web Developer can learn this.");
 		return hm;
+	}
+
+
+	@RequestMapping(value="/add",method= RequestMethod.POST)
+	public Home addData(@RequestBody Home home){
+		return home;
 	}
 
 
